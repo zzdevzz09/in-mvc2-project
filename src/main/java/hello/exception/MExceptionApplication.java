65e2f5b6a5f746f3f985abd87b91957e854af3f4
Jeyun Park <jy095201@gmail.com>
2023-07-18 08:14:18 +0900

    init commit

---
+++ src/main/java/hello/exception/MExceptionApplication.java
@@ -0,0 +1,13 @@
+package hello.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MExceptionApplication.class, args);
+	}
+
+}
