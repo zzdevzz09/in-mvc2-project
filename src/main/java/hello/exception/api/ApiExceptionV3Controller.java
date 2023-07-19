439440f174d6f853d821f45c2a7478a4e195b648
Jeyun Park <jy095201@gmail.com>
2023-07-20 08:37:32 +0900

    api error

---
+++ src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -0,0 +1,43 @@
+package hello.exception.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hello.exception.api.ApiExceptionV2Controller.MemberDto;
+import hello.exception.exception.UserException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/api3")
+@RestController
+public class ApiExceptionV3Controller {
+
+	@GetMapping("/members/{id}")
+	public MemberDto getMember(@PathVariable("id") String id) {
+		
+		if(id.equals("ex")) {
+			throw new RuntimeException("잘못된 사용자");
+		}
+
+		if(id.equals("bad")) {
+			throw new IllegalArgumentException("잘못된 입력 값");
+		}
+		
+		if(id.equals("user-ex")) {
+			throw new UserException("사용자 오류");
+		}
+		
+		return new MemberDto(id, "hello " + id);
+	}
+	
+	@Data
+	@AllArgsConstructor
+	static class MemberDto{
+		private String memberId;	
+		private String name;
+	}
+}
